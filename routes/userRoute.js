7bb2210691d04eae1d75c4916de9256967bc5156
Amadeus Her <Amadeusherr@gmail.com>
2020-12-23 21:43:37 -0600

    change userRouter, as some routes are not being used

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -30,35 +30,6 @@ router.get("/", async (request, response) => {
     }
 })
 
-router.delete("/:id", async (request, response) => {
-    try{
-        let user = await User.findById(request.params.id);
-        await cloudinary.uploader.destroy(user.cloudinary_id);
-        await user.remove();
-        response.json(user);
-    }catch(error){
-        console.log(error);
-    }
-})
-
-router.put("/:id", upload.single("image"), async (req, res) => {
-    try {
-        let user = await User.findById(req.params.id);
-        // Delete image from cloudinary
-        await cloudinary.uploader.destroy(user.cloudinary_id);
-        // Upload image to cloudinary
-        const result = await cloudinary.uploader.upload(req.file.path);
-        const data = {
-        name: req.body.name || user.name,
-        avatar: result.secure_url || user.avatar,
-        cloudinary_id: result.public_id || user.cloudinary_id,
-        };
-        user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
-        res.json(user);
-    } catch (err) {
-        console.log(err);
-    }
-});
 
 module.exports = router;
   
